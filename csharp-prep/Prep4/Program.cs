94da645606968467028222f3e58da1657d1129e9
Cartier3 <chart2021@byui.edu>
2025-01-15 18:57:13 -0700

    Prep 4 Completion!

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Formats.Asn1;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+       
+        int defVar = 1;
+        List<int> numbers = new List<int>();
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        while(defVar != 0)
+        {
+           
+            Console.WriteLine($"Enter number: "); 
+            int uNumber = int.Parse(Console.ReadLine());
+            
+
+            if (uNumber != 0)
+            {
+                numbers.Add(uNumber);
+            }
+
+            if(uNumber == 0)
+            {
+                defVar = 0;
+                
+                float sum = numbers.Sum();
+                int count = numbers.Count();
+                float avg = (sum / count);
+                int max = numbers.Max();
+
+                Console.WriteLine("The sum is:" + sum);
+                Console.WriteLine("The average is:" + avg);
+                Console.WriteLine("The largest number is:" + max);
+                Console.WriteLine("End Program.");
+            }
+        }
+        
+        
+    
     }
 }
