0a1d0c62ef56789b54acd94875e8e06c12a4f7a6
Cartier3 <chart2021@byui.edu>
2025-03-06 15:53:46 -0700

    Prepare Week 09 Code Completion

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        
+        List<Shape> shapes = new List<Shape>();
+
+        Square squareShape = new Square("blue", 8.8);
+        
+        Console.WriteLine($"\n{squareShape.GetColor()}");
+        Console.WriteLine($"{squareShape.GetArea()}");
+
+        Circle circleShape = new Circle("green", 5.5);
+
+        Console.WriteLine($"\n{circleShape.GetColor()}");
+        Console.WriteLine($"{circleShape.GetArea()}");
+
+        Rectangle rectShape = new Rectangle("pink", 3, 5);
+
+        Console.WriteLine($"\n{rectShape.GetColor()}");
+        Console.WriteLine($"{rectShape.GetArea()}");
+
+        shapes.Add(squareShape);
+        shapes.Add(circleShape);
+        shapes.Add(rectShape);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"\nThe {color} shape has an area of {area}.");
+        }
+        
     }
 }
